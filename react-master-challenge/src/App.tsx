c048536361cee38a0a97bb43c8c9b3b27d50961e
hyunjun00 <parkhyunjun00@gmail.com>
2025-01-21 11:14:56 +0900

    #3.3 Optional Props

--- react-master-challenge/src/App.tsx
+++ react-master-challenge/src/App.tsx
@@ -4,8 +4,8 @@ import Circle from "./Circle.tsx";
 function App() {
   return (
     <div>
-      <Circle bgColor="teal" />
-      <Circle bgColor="tomato" />
+      <Circle borderColor="blue" bgColor="teal" />
+      <Circle text="Im here" bgColor="tomato" />
     </div>
   );
 }
