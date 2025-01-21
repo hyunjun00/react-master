73db54c581d611bc15180b868ec52f092e05984f
hyunjun00 <parkhyunjun00@gmail.com>
2025-01-21 14:04:02 +0900

    #3.6 Themes

--- react-master-challenge/src/index.tsx
+++ react-master-challenge/src/index.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import { ThemeProvider } from "styled-components";
+import { darkTheme, lightTheme } from "./theme.ts";
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={darkTheme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
