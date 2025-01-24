96968063c0e6dbb1042a9295c26e9dedce50a8b6
hyunjun00 <parkhyunjun00@gmail.com>
2025-01-25 00:36:27 +0900

    candlestick Chart

--- react-master-challenge/src/routes/Chart.tsx
+++ react-master-challenge/src/routes/Chart.tsx
@@ -32,17 +32,80 @@ function Chart() {
   );
 
   const isDark = useRecoilValue(isDarkAtom);
+  const ohlcvData = data ?? [];
+  const dayData = ohlcvData.map((day) => {
+    return {
+      x: day.time_close,
+      y: [day.open, day.high, day.low, day.close],
+    };
+  });
   return (
     <div>
       {isLoading ? (
         "Loading chart..."
       ) : (
+        // <ApexCharts
+        //   type="line"
+        //   series={[
+        //     {
+        //       name: "Price",
+        //       data: data?.map((price) => price.close) as number[],
+        //     },
+        //   ]}
+        //   options={{
+        //     theme: {
+        //       mode: isDark ? "dark" : "light",
+        //     },
+        //     chart: {
+        //       height: 300,
+        //       width: 500,
+        //       toolbar: {
+        //         show: false,
+        //       },
+        //       background: "transparent",
+        //     },
+        //     grid: {
+        //       show: false,
+        //     },
+        //     stroke: {
+        //       curve: "smooth",
+        //       width: 4,
+        //     },
+        //     yaxis: {
+        //       show: false,
+        //     },
+        //     xaxis: {
+        //       axisBorder: { show: false },
+        //       axisTicks: { show: false },
+        //       labels: {
+        //         show: false,
+        //       },
+        //       type: "datetime",
+        //       categories: data?.map((price) =>
+        //         new Date(price.time_close * 1000).toUTCString()
+        //       ),
+        //     },
+        //     fill: {
+        //       type: "gradient",
+        //       gradient: {
+        //         gradientToColors: ["#0be881"],
+        //         stops: [0, 100],
+        //       },
+        //     },
+        //     colors: ["#0fbcf9"],
+        //     tooltip: {
+        //       y: {
+        //         formatter: (value) => `$${value.toFixed(2)}`,
+        //       },
+        //     },
+        //   }}
+        // />
         <ApexCharts
-          type="line"
+          type="candlestick"
           series={[
             {
               name: "Price",
-              data: data?.map((price) => price.close) as number[],
+              data: dayData,
             },
           ]}
           options={{
