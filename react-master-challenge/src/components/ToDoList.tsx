1a64ce6bc263051fa4c570a71ffd8833b8029078
hyunjun00 <parkhyunjun00@gmail.com>
2025-01-31 14:10:34 +0900

    #6.16 Selectors part One

--- react-master-challenge/src/components/ToDoList.tsx
+++ react-master-challenge/src/components/ToDoList.tsx
@@ -1,21 +1,37 @@
 import React from "react";
 import { useRecoilValue } from "recoil";
 import CreateToDo from "./CreateToDo.tsx";
-import { toDoState } from "../atoms.ts";
+import { toDoSelector, toDoState } from "../atoms.ts";
 import ToDo from "./ToDo.tsx";
 
 function ToDoList() {
-  const toDos = useRecoilValue(toDoState);
+  const [toDo, doing, done] = useRecoilValue(toDoSelector);
   return (
     <div>
       <h1>To Dos</h1>
       <hr />
       <CreateToDo />
+      <h2>To Do</h2>
       <ul>
-        {toDos.map((toDo) => (
+        {toDo.map((toDo) => (
           <ToDo key={toDo.id} {...toDo} />
         ))}
       </ul>
+      <hr />
+      <h2>Doing</h2>
+      <ul>
+        {doing.map((toDo) => (
+          <ToDo key={toDo.id} {...toDo} />
+        ))}
+      </ul>
+      <hr />
+      <h2>Done</h2>
+      <ul>
+        {done.map((toDo) => (
+          <ToDo key={toDo.id} {...toDo} />
+        ))}
+      </ul>
+      <hr />
     </div>
   );
 }
