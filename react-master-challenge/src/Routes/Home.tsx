6214a5546d3e270318401262083a11f142893cbd
hyunjun00 <parkhyunjun00@gmail.com>
2025-02-02 16:22:36 +0900

    #9.10 Box Animations part Two

--- react-master-challenge/src/Routes/Home.tsx
+++ react-master-challenge/src/Routes/Home.tsx
@@ -66,6 +66,19 @@ const Box = styled(motion.div)<{ $bgPhoto: string }>`
   }
 `;
 
+const Info = styled(motion.div)`
+  padding: 10px;
+  background-color: ${(props) => props.theme.black.lighter};
+  opacity: 0;
+  position: absolute;
+  width: 100%;
+  bottom: 0;
+  h4 {
+    text-align: center;
+    font-size: 18px;
+  }
+`;
+
 const rowVariants = {
   hidden: {
     x: window.outerWidth + 5,
@@ -84,16 +97,27 @@ const boxVarinats = {
   },
   hover: {
     scale: 1.3,
-    y: -50,
+    y: -80,
     transition: {
       delay: 0.5,
-      duration: 0.3,
+      duration: 0.1,
       type: "tween",
     },
   },
 };
 
 const offset = 6;
+
+const infoVariants = {
+  hover: {
+    opacity: 1,
+    transition: {
+      delay: 0.5,
+      duration: 0.1,
+      type: "tween",
+    },
+  },
+};
 
 function Home() {
   const { data, isLoading } = useQuery<IGetMoviesResult>({
@@ -146,7 +170,11 @@ function Home() {
                       variants={boxVarinats}
                       transition={{ type: "tween" }}
                       $bgPhoto={makeImagePath(movie.backdrop_path, "w500")}
-                    />
+                    >
+                      <Info variants={infoVariants}>
+                        <h4>{movie.title}</h4>
+                      </Info>
+                    </Box>
                   ))}
               </Row>
             </AnimatePresence>
