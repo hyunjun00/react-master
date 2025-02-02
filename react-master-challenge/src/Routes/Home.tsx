c5bd3bbe33b965a161ea30dbfa5c761f04429d4a
hyunjun00 <parkhyunjun00@gmail.com>
2025-02-02 14:53:07 +0900

    #9.6 Home Screen part Two

--- react-master-challenge/src/Routes/Home.tsx
+++ react-master-challenge/src/Routes/Home.tsx
@@ -1,14 +1,61 @@
 import React from "react";
 import { useQuery } from "react-query";
-import { getMovies } from "../api.ts";
+import { getMovies, IGetMoviesResult } from "../api.ts";
+import styled from "styled-components";
+import { makeImagePath } from "../utils.ts";
+
+const Wrapper = styled.div`
+  background-color: black;
+`;
+
+const Loader = styled.div`
+  height: 20vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Banner = styled.div<{ $bgPhoto: string }>`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 60px;
+  background-image: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 1)),
+    url(${(props) => props.$bgPhoto});
+  background-size: cover;
+`;
+
+const Title = styled.h2`
+  font-size: 68px;
+  margin-bottom: 20px;
+`;
+
+const Overview = styled.p`
+  font-size: 30px;
+  width: 50%;
+`;
 
 function Home() {
-  const { data, isLoading } = useQuery({
+  const { data, isLoading } = useQuery<IGetMoviesResult>({
     queryKey: ["movies", "nowPlaying"],
     queryFn: getMovies,
   });
   return (
-    <div style={{ backgroundColor: "whitesmoke", height: "200vh" }}>home</div>
+    <Wrapper>
+      {isLoading ? (
+        <Loader>Loading...</Loader>
+      ) : (
+        <>
+          <Banner
+            $bgPhoto={makeImagePath(data?.results[0].backdrop_path || "")}
+          >
+            <Title>{data?.results[0].title}</Title>
+            <Overview>{data?.results[0].overview}</Overview>
+          </Banner>
+        </>
+      )}
+    </Wrapper>
   );
 }
 
